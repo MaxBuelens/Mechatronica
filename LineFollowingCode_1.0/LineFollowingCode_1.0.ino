95222e0dbe480dc5f3ec011f58775d0116825497
MaxBuelensTM <72995763+MaxBuelensTM@users.noreply.github.com>
2020-10-16 21:02:19 +0200

    Update LineFollowingCode_1.0.ino
    16/10/2020

--- LineFollowingCode_1.0/LineFollowingCode_1.0.ino
+++ LineFollowingCode_1.0/LineFollowingCode_1.0.ino
@@ -1,12 +1,74 @@
 /*Line Following code */
-//Test
+
+//Analoge
+const int Drukknop = A0;
+const int Sensor1 = A1;
+const int Sensor2 = A2;
+const int Sensor3 = A3;
+const int Sensor4 = A4;
+const int Sensor5 = A5;
+
+//Digitale
+Const int Enable = 13;
+Const int Motor1L = 5;
+Const int Motor1R = 6; //Ik ben niet zeker meer welke pinnen het waren
+Const int Motor2L = 10;
+Const int Motor2R = 11;
+
+//Drukknop variabele
+int DrukknopValue = 0;
+bool Active = false;
+
+//Sensoren variabele
+int Sensor1Value = 0;
+int Sensor2Value = 0;
+int Sensor3Value = 0;
+int Sensor4Value = 0;
+int Sensor5Value = 0;
+
 
 void setup() {
-  // put your setup code here, to run once:
+ // Voor de serial poort te kunnen gebruiken
+ Serial.Begin(9600);
+ 
+ // Digitale pinnen als output zetten
+ pinMode(Drukknop, INPUT);
+ pinMode(Enable, OUTPUT);
+ pinMode(Motor1L, OUTPUT);
+ pinMode(Motor1R, OUTPUT);
+ pinMode(Motor2L, OUTPUT);
+ pinMode(Motor2R, OUTPUT);
+   
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //Lees de drukknop
+  DrukknopValue = digitalRead(Drukknop);
+  
+   // Lees de analoge ingangen en schrijf de data weg
+  Sensor1Value = analogRead(Sensor1);
+  // Geef even tijd om te lezen en weg te schrijven
+  Delay(5);
+  Sensor2Value = analogRead(Sensor2);
+  Delay(5);
+  Sensor3Value = analogRead(Sensor3);
+  Delay(5);
+  Sensor4Value = analogRead(Sensor4);
+  Delay(5);
+  Sensor5Value = analogRead(Sensor5);
+  Delay(5);
+  
+  // Drukknop gedrukt start programma
+  if (DrukknopValue == HIGH){
+    Active =! Active;    
+  } 
+  
+  // Doorloop het programma
+  If Active{
+    // Hier de werkelijke code
+  }
 
 }
+
+
