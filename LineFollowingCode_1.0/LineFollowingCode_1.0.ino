13537f31cc37a1e811a81bd51f5808b6da5f4937
MaxBuelensTM <72995763+MaxBuelensTM@users.noreply.github.com>
2020-10-19 12:14:26 +0200

    Update LineFollowingCode_1.0.ino

--- LineFollowingCode_1.0/LineFollowingCode_1.0.ino
+++ LineFollowingCode_1.0/LineFollowingCode_1.0.ino
@@ -9,11 +9,11 @@ const int Sensor4 = A4;
 const int Sensor5 = A5;
 
 //Digitale
-Const int Enable = 13;
-Const int Motor1CW = 5; // Draaizin motoren nog niet zeker
-Const int Motor1CCW = 10;
-Const int Motor2CW = 6;
-Const int Motor2CCW = 11;
+const int Enable = 13;
+const int Motor1CW = 5; // Draaizin motoren nog niet zeker
+const int Motor1CCW = 10;
+const int Motor2CW = 6;
+const int Motor2CCW = 11;
 
 //Drukknop variabele
 int DrukknopValue = 0;
@@ -22,91 +22,163 @@ bool Active = false;
 //Sensoren variabele
 int Sensor1Value = 0;
 int Sensor2Value = 0;
-int Sensor3Value = 0;
+int Sensor3Value = 50;
 int Sensor4Value = 0;
 int Sensor5Value = 0;
 
 
 void setup() {
- // Voor de serial poort te kunnen gebruiken
- Serial.Begin(9600);
- 
- // Drukknop als input
- pinMode(Drukknop, INPUT);
- 
- // Digitale pinnen als output
- pinMode(Enable, OUTPUT);
- pinMode(Motor1CW, OUTPUT);
- pinMode(Motor1CCW, OUTPUT);
- pinMode(Motor2CW, OUTPUT);
- pinMode(Motor2CCW, OUTPUT);
-   
+  // Voor de serial poort te kunnen gebruiken
+  Serial.begin(9600);
+
+  // Drukknop als input
+  pinMode(Drukknop, INPUT);
+
+  // Digitale pinnen als output
+  pinMode(Enable, OUTPUT);
+  pinMode(Motor1CW, OUTPUT);
+  pinMode(Motor1CCW, OUTPUT);
+  pinMode(Motor2CW, OUTPUT);
+  pinMode(Motor2CCW, OUTPUT);
+
 
 } //End Setup
 
 
 void loop() {
-  // Read the sensors and button een overzichterlijke weergave door meerdere voids te gebruiken
   ReadSensorAndButton();
-  // Doorloop het programma
-  if Active{
-    // Hier de werkelijke code
-  } // End if active
+
+  if (Active) {
+    if (Sensor1Value > 500 && Sensor2Value > 500 && Sensor3Value < 100 && Sensor4Value > 500 && Sensor5Value > 500) {
+      Forward();
+    }
+
+    if (Sensor1Value > 500 && Sensor2Value < 100 && Sensor3Value < 100 && Sensor4Value > 500 && Sensor5Value > 500) {
+      TrunLeft();
+    }
+
+    if (Sensor1Value > 500 && Sensor2Value > 500 && Sensor3Value < 100 && Sensor4Value < 100 && Sensor5Value > 500) {
+      TrunRight();
+    }
+
+    if (Sensor1Value < 100 && Sensor2Value < 100 && Sensor3Value < 100 && Sensor4Value > 500 && Sensor5Value > 500) {
+      SharpTrunLeft();
+    }
+
+    if (Sensor1Value > 500 && Sensor2Value > 500 && Sensor3Value < 100 && Sensor4Value < 100 && Sensor5Value < 100) {
+      SharpTrunRight();
+    }
+
+    if (Sensor1Value < 100 && Sensor2Value < 100 && Sensor3Value < 100 && Sensor4Value < 100 && Sensor5Value < 100) {
+      Stop();
+    }
+  }//If active
+
+  else {
+    digitalWrite(Enable, LOW);
+  }
+
 }//End Void loop
 
 
-#######################################################################################################################################
-// Read the sensors and button 
-Void ReadSensorsAndButton(){
-   //Lees de drukknop
+//#######################################################################################################################################
+// Read the sensors and button
+void ReadSensorAndButton() {
+
+  //Lees de drukknop
   DrukknopValue = digitalRead(Drukknop);
-  
+
   // Lees de analoge ingangen en schrijf de data weg
   Sensor1Value = analogRead(Sensor1);
   // Geef even tijd om te lezen en weg te schrijven
-  Delay(5);
+  delay(5);
   Sensor2Value = analogRead(Sensor2);
-  Delay(5);
-  Sensor3Value = analogRead(Sensor3);
-  Delay(5);
+  delay(5);
+  //Sensor3Value = analogRead(Sensor3);
+  // delay(5);
   Sensor4Value = analogRead(Sensor4);
-  Delay(5);
+  delay(5);
   Sensor5Value = analogRead(Sensor5);
-  Delay(5);
- 
- // Serial print 
- Serial.print("Sensor1:" + Sensor1Value);
- Serial.print("\t");
- Serial.print("Sensor2:" + Sensor2Value);
- Serial.print("\t");
- Serial.print("Sensor3:" + Sensor3Value);
- Serial.print("\t");
- Serial.print("Sensor4:" + Sensor4Value);
- Serial.print("\t");
- Serial.println("Sensor5:" + Sensor5Value);
- 
-  // Drukknop gedrukt start programma
-  if (DrukknopValue == HIGH){
-    Active =! Active; 
+  delay(5);
+
+  Serial.print("Sensor1:");
+  Serial.print(Sensor1Value);
+  Serial.print("\t");
+  Serial.print("Sensor2:");
+  Serial.print(Sensor2Value);
+  Serial.print("\t");
+  Serial.print("Sensor3:");
+  Serial.print(Sensor3Value);
+  Serial.print("\t");
+  Serial.print("Sensor4:");
+  Serial.print(Sensor4Value);
+  Serial.print("\t");
+  Serial.print("Sensor5:");
+  Serial.println(Sensor5Value);
+
+  if (DrukknopValue == LOW) {
+    Active = ! Active;
     Serial.println("Push on button ==> robot is active");
-    Delay(200);
-  } // End if DrukknopValue
+    delay(200);
+  }
+
 } //End Void ReadSensorAndButton
-#######################################################################################################################################
+//#######################################################################################################################################
 // Go forward
-Void Forward(){
- 
+void Forward() {
+  digitalWrite(Enable, HIGH);
+  analogWrite(Motor1CW, 200);
+  analogWrite(Motor1CCW, 0);
+  analogWrite(Motor2CW, 200);
+  analogWrite(Motor2CCW, 0);
+  Serial.println("Forward");
 }//Void Forward
-#######################################################################################################################################
+//#######################################################################################################################################
 //Go left
-Void TrunLeft(){
- 
+void TrunLeft() {
+  digitalWrite(Enable, HIGH);
+  analogWrite(Motor1CW, 150);
+  analogWrite(Motor1CCW, 0);
+  analogWrite(Motor2CW, 200);
+  analogWrite(Motor2CCW, 0);
+  Serial.println("Left");
 }//Void TrunLeft
-#######################################################################################################################################
+//#######################################################################################################################################
 // Go right
-Void TrunRight(){
- 
+void TrunRight() {
+  /*digitalWrite(Enable, HIGH);
+    analogWrite(Motor1CW, 200);
+    analogWrite(Motor1CCW, 0);
+    analogWrite(Motor2CW, 150);
+    analogWrite(Motor2CCW, 0);*/
+  Serial.println("Right");
 } //Void TrunRight
-#######################################################################################################################################
+//#######################################################################################################################################
 
+void SharpTrunLeft() {
+  /* digitalWrite(Enable, HIGH);
+    analogWrite(Motor1CW, 0);
+    analogWrite(Motor1CCW, 0);
+    analogWrite(Motor2CW, 200);
+    analogWrite(Motor2CCW, 0);*/
+  Serial.println("SharpTurnLeft");
+}
 
+void SharpTrunRight() {
+  /*digitalWrite(Enable, HIGH);
+    analogWrite(Motor1CW, 200);
+    analogWrite(Motor1CCW, 0);
+    analogWrite(Motor2CW, 0);
+    analogWrite(Motor2CCW, 0);*/
+  Serial.println("SharpTurnRight");
+}
+
+//#######################################################################################################################################
+void Stop() {
+  digitalWrite(Enable, LOW);
+  analogWrite(Motor1CW, 0);
+  analogWrite(Motor1CCW, 0);
+  analogWrite(Motor2CW, 0);
+  analogWrite(Motor2CCW, 0);
+  Serial.println("Stop");
+}
