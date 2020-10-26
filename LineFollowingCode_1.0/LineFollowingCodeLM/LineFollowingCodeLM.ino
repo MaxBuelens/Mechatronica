bf33f3fc4aac9f70f50ce78f1b0be3e56cd8e3db
MadeByMax <42475109+MaxBuelens@users.noreply.github.com>
2020-10-26 10:52:48 +0100

    Update LineFollowingCodeLM.ino

--- LineFollowingCode_1.0/LineFollowingCodeLM/LineFollowingCodeLM.ino
+++ LineFollowingCode_1.0/LineFollowingCodeLM/LineFollowingCodeLM.ino
@@ -66,7 +66,7 @@ void loop() {
     //zwart / zwart / Zwart / wit / wit
     if (Sensor1Value < 100 && Sensor2Value < 100 && Sensor3Value < 100 && Sensor4Value > 500 && Sensor5Value > 500) {
       // Doe dit tot robot weer op recht op lijn zit anders blijven draaien
-      //Nog geen test
+      //Nog een test
       while (!(Sensor2Value > 500 && Sensor3Value < 100 && Sensor4Value > 500) || !Active) {
         ReadSensorAndButton();
         SharpTrunLeft();
